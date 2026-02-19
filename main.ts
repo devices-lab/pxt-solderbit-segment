4dee159c8ef30649a8685dfe0f25f374a09ad08d
KierPalin <45743174+KierPalin@users.noreply.github.com>
2026-02-19 15:30:19 +0000

    showNumber: DOES basic.pause() if it's a single digit

--- main.ts
+++ main.ts
@@ -118,17 +118,14 @@ namespace solderbit_segment {
           false
         )
 
-        // Don't wait if it's  single digit:
-        if (numAsString.length > 1) {
-          // Don't do the 'turn display off briefly effect' if perDigitWaitTimeMS is too low
-          const offFlashTimeMS = 250;
-          if (perDigitWaitTimeMS <= offFlashTimeMS) {
-            basic.pause(perDigitWaitTimeMS)
-          } else { // Turn the display off for offFlashTimeMS
-            basic.pause(Math.max(perDigitWaitTimeMS - offFlashTimeMS, offFlashTimeMS))
-            clear();
-            basic.pause(offFlashTimeMS)
-          }
+        // Don't do the 'turn display off briefly effect' if perDigitWaitTimeMS is too low
+        const offFlashTimeMS = 250;
+        if (perDigitWaitTimeMS <= offFlashTimeMS) {
+          basic.pause(perDigitWaitTimeMS)
+        } else { // Turn the display off for offFlashTimeMS
+          basic.pause(Math.max(perDigitWaitTimeMS - offFlashTimeMS, offFlashTimeMS))
+          clear();
+          basic.pause(offFlashTimeMS)
         }
       }
     }
