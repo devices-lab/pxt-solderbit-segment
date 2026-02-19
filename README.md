c666b4b800452e5610f691e78f85d069c399b38b
Aron Eggens <aroneggens@gmail.com>
2026-02-19 13:41:03 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# MakeCode extension for the solder:bit Segment
+
+## Use this extension
+
+1. Open [MakeCode](https://makecode.microbit.org/) and create a new project, or open an exisitng one.
+2. Click on `Extensions` under the gearwheel menu.
+
+![MakeCode](/guide/makecode.png)
+
+3. Search for `https://github.com/devices-lab/pxt-solderbit-segment`
+
+![Searcing for extensions in MakeCode](/guide/search.png)
+
+4. Select `solderbit-segment`
+
+![Selecting the extension in MakeCode](/guide/solderbit-segment-search.png)
+
+## Editing this extension in Makecode
+
+You may edit or remix this project directly in MakeCode.
+
+1. Open [MakeCode](https://makecode.microbit.org/).
+2. Click on `Import` then click on `Import URL`.
+3. Paste `https://github.com/devices-lab/pxt-solderbit-segment` and click import.
+
+#### Metadata (used for search, rendering)
+
+- for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,96 @@
+
+/**
+* Displayable digits for the 8 segment + decimal point VDMO10A0 display.
+* 0 to 9 and the decimal point
+*/
+enum Digit {
+  Zero = 0xC0,        // 0: A B C D E F on, G off
+  //% block="one"
+  One = 0xF9,         // 1: B C on
+  //% block="two"
+  Two = 0xA4,         // 2: A B D E G on
+  //% block="three"
+  Three = 0xB0,       // 3: A B C D G on
+  //% block="four"
+  Four = 0x99,        // 4: B C F G on
+  //% block="five"
+  Five = 0x92,        // 5: A C D F G on
+  //% block="six"
+  Six = 0x82,         // 6: A C D E F G on
+  //% block="seven"
+  Seven = 0xF8,       // 7: A B C on
+  //% block="eight"
+  Eight = 0x80,       // 8: all segments on (except D.P)
+  //% block="nine"
+  Nine = 0x90,        // 9: A B C D F G onxq
+  //% block="decimal point"
+  Point = 0x7F        // .: A B C D E F G off, D.P. on
+}
+
+/**
+* Which IO Expander is soldered down?
+* The SO-16 PCF8754AT (has I2C address 0x20) or the DIP-16 PCF8754N (has I2C address 0x18)?
+* You must invoke solderbit_segment.setup(IO_Expander) with your config to use this extension.
+* Otherwise you will get an error since IO_Expander.NONE will is used by default.
+*/
+enum IO_Expander {
+  //% block="NONE"
+  NONE,
+  //% block="PCF8754AT"
+  PCF8754AT = 0x20,
+  //% block="PCF8754N"
+  PCF8754N = 0x38,
+}
+
+/**
+ * solder:bit segment
+ */
+//% block="solderbit Segment" weight=100 color=#000000 icon="\uf10b"
+namespace solderbit_segment {
+  let selectedI2CAddr: IO_Expander = IO_Expander.NONE;
+
+  /**
+   * You must invoke this before using show or clear.
+   * Which IO_Expander are you using?
+   * This is important because they have different i2c addresses.
+   * @param ioExpander component from the IO_Expander enum
+   */
+  //% block
+  export function init(ioExpander: IO_Expander): void {
+    selectedI2CAddr = ioExpander;
+    // Set all 8 pins of the IO Expander to output.
+    // This isn't strictly necessary in our case.
+    pins.i2cWriteNumber(
+      selectedI2CAddr,
+      0b00000000,
+      NumberFormat.Int8LE,
+      false
+    )
+    // clear();
+  }
+
+  /*
+   * Internal lookup function to convert a single digit into the Digit enum.
+   * Unfortunately this switch is the best way to do this on account of STS enum constraints.
+   * Used by show_number
+   * @throws error if single_digit_num is not a single digit.
+   */
+  function digitLookup(single_digit_num: string): Digit {
+    switch (single_digit_num) {
+      case "0": return Digit.Zero;
+      case "1": return Digit.One;
+      case "2": return Digit.Two;
+      case "3": return Digit.Three;
+      case "4": return Digit.Four;
+      case "5": return Digit.Five;
+      case "6": return Digit.Six;
+      case "7": return Digit.Seven;
+      case "8": return Digit.Eight;
+      case "9": return Digit.Nine;
+      case ".": return Digit.Point;
+      default: throw "solderbit_segment internal fn digit_lookup: error parsing single digit " + single_digit_num
+    }
+  }
+
+
+}
